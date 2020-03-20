f436a0b828f47cf076aa6eb30a50085e7396d2f4
David Edler <david.edler@researchgate.net>
2020-03-20 21:32:20 +0100

    use postgres instead of sqlite

--- farmhelden/farmhelden/settings.py
+++ farmhelden/farmhelden/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'farmhelden.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'farmhelden',
+        'USER': 'farmheldenuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 

a618ca016fbd639feb1846d268137ad51aea62cf
Skolik <andrea.skolik@volkswagen.de>
2020-03-21 15:35:27 +0100

    added Point fields, user type choice fields

--- farmhelden/rest_server/models.py
+++ farmhelden/rest_server/models.py
@@ -1,3 +1,4 @@
+from django.contrib.gis.geos import Point
 from django.db import models
 
 
@@ -7,17 +8,20 @@ class UserType(models.Model):
 
 
 class User(models.Model):
+    USER_CHOICES = (
+        ('1', 'Landwirt'),
+        ('2', 'Helfer'))
+
     id = models.AutoField(primary_key=True)
-    zip_code = models.CharField(max_length=10)
+    point = Point()
     has_license = models.BooleanField(null=True)
-    user_type = models.ForeignKey(UserType, null=True, on_delete=models.SET_NULL)
+    user_type = models.CharField(max_length=1, choices=USER_CHOICES)
 
 
 class Farm(models.Model):
     id = models.AutoField(primary_key=True)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    zip_code = models.CharField(max_length=10)
-    street = models.CharField(max_length=30, null=True, blank=True)
+    point = Point()
 
 
 class Job(models.Model):
@@ -28,16 +32,16 @@ class Job(models.Model):
     date_to = models.DateTimeField()
 
 
-class Task(models.Model):
-    id = models.AutoField(primary_key=True)
-    job = models.ForeignKey(Job, on_delete=models.CASCADE)
-    needs_license = models.BooleanField()
-    users = models.ManyToManyField(User)
-
-
 class TaskType(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
+    description = models.TextField(max_length=1000)
 
 
-
+class Task(models.Model):
+    id = models.AutoField(primary_key=True)
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    task_type = models.ForeignKey(TaskType, null=True, on_delete=models.SET_NULL)
+    needs_license = models.BooleanField()
+    users = models.ManyToManyField(User)
+    description = models.TextField(max_length=1000)

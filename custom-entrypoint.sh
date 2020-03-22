9f5d7d5e28068986f1937139704715bc9cd75cf1
Florian Gerhardt <florian.gerhardt92@gmail.com>
2020-03-22 01:06:10 +0100

    Use gunicorn for starting the production server

--- custom-entrypoint.sh
+++ custom-entrypoint.sh
@@ -10,15 +10,25 @@ wait_for_postgres() {
     echo "\nPostgres is available"
 }
 
-start_server() {
+run_development() {
     python manage.py makemigrations
     python manage.py migrate
     python manage.py runserver 0.0.0.0:8000
 }
 
+start_server() {
+  python manage.py migrate
+  gunicorn -w 3 -b 0.0.0.0:8000 farmhelden.wsgi
+}
+
 if [ "$1" = 'production' ]; then
     wait_for_postgres
     start_server
+fi
+
+if [ "$1" = 'development' ]; then
+    wait_for_postgres
+    run_development
 fi
 
 exec "$@"

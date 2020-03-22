59ddf6861ad4085349b7789af4fd782445452b1d
Florian Gerhardt <florian.gerhardt92@gmail.com>
2020-03-22 13:48:12 +0100

    Add helper functions to seed file

--- farmhelden/rest_server/management/commands/seedDbData.py
+++ farmhelden/rest_server/management/commands/seedDbData.py
@@ -1,21 +1,35 @@
 from django.core.management.base import BaseCommand, CommandError
 from rest_server.models import User, Farm, Location, Campaign
 from django.utils import timezone
+from django.contrib.gis.geos import Point
 import datetime
 
 import pytz
 
+
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        try:
-            myUser = User.objects.get(email="foo@bar.com")
-        except:
-            myUser = User.objects.create(email="foo@bar.com", password="password")
-
-        myFarm = Farm.objects.create(street="Bahnhofstrasse 22", zip_code="12345", user_id=myUser)
-        myLocation = Location.objects.create(info="Ein schöner ort", farm_id=myFarm, location_type=1)
-
         start = pytz.utc.localize(datetime.datetime(2020, 5, 1, 0, 0))
         end = pytz.utc.localize(datetime.datetime(2020, 5, 5, 0, 0))
-        myCampaign = Campaign.objects.create(farm_id=myFarm, location_id=myLocation, date_from=start, date_to=end)
+        self.create_user_with_farms_and_location(self, email="foo@bar.com", password="password",
+                                                 street="Am Deich 2", zip_code="25797",
+                                                 info="Hof Haase", date_from=start, date_to=end, point=Point(9.010076, 54.144092))
+
+    def create_users_with_farms_locations_and_campaign(self, email, password, street, zip_code, info, date_from,
+                                                       date_to, point, location_type=1):
+        try:
+            user = User.objects.get(email=email)
+        except:
+            user = User.objects.create(email=email, password=password)
+
+        farm = Farm.objects.create(street=street, zip_code=zip_code, user_id=user)
+        location = Location.objects.create(info=info, farm_id=farm, location_type=location_type, point=point)
+
+        self.create_campaign(self, farm=farm, location=location, date_from=date_from, date_to=date_to)
+
+        return user, farm, location
+
+    def create_campaign(self, farm, location, date_from, date_to):
+        campaign = Campaign.objects.create(farm_id=farm, location_id=location, date_from=date_from, date_to=date_to)
+        return campaign
